3c0216911f5f363e98bf60817223c2818f959c98
Sondos Ahmed Ibrahim <sunnysondos42@gmail.com>
2022-09-03 12:12:01 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a,b,c){
+    a + b 
+    return a + b
+}
+function subtract(a,b){
+    a - b
+    return a - b
+}
+function multiply(a,b){
+    a * b
+    return a * b
+}
+function divide(a,b){
+    a / b
+    return a / b 
+}
+function increment(number){
+    return number +=1; 
+
+}
+function decrement(number){
+    return number -=1;
+}
+function makeInt(n){
+
+    return parseInt(n,10)
+}
+function preserveDecimal(n){
+    return parseFloat(n,10)
+}
